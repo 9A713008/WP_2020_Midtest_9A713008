b1039fe3082f81bb13653bcac634aba98cf9fcc0
張秤羿 <badthing0102@gmail.com>
2020-11-19 16:27:14 +0800

    加入專案檔案。

---
+++ WP_2020_Midtest_9A713008/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WP_2020_Midtest_9A713008
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lab1 = new System.Windows.Forms.Label();
+            this.btn3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("微軟正黑體", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn1.Location = new System.Drawing.Point(128, 309);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(136, 100);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "玩家一";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("微軟正黑體", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn2.Location = new System.Drawing.Point(495, 309);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(136, 100);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "玩家二";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WP_2020_Midtest_9A713008.Properties.Resources.P0;
+            this.pictureBox1.Location = new System.Drawing.Point(90, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(212, 193);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WP_2020_Midtest_9A713008.Properties.Resources.P0;
+            this.pictureBox2.Location = new System.Drawing.Point(463, 110);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(197, 193);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lab1
+            // 
+            this.lab1.AutoSize = true;
+            this.lab1.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lab1.Location = new System.Drawing.Point(138, 36);
+            this.lab1.Name = "lab1";
+            this.lab1.Size = new System.Drawing.Size(69, 35);
+            this.lab1.TabIndex = 4;
+            this.lab1.Text = "測試";
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("新細明體", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn3.Location = new System.Drawing.Point(327, 330);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(114, 78);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "reset";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.lab1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lab1;
+        private System.Windows.Forms.Button btn3;
+    }
+}
+
---
+++ WP_2020_Midtest_9A713008/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WP_2020_Midtest_9A713008.Properties;
+
+namespace WP_2020_Midtest_9A713008
+{
+    public partial class Form1 : Form
+    {
+        List<Image> list = new List<Image>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+        #region 生命週期
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            list.Add(Resources.P0);
+            list.Add(Resources.P1);
+            list.Add(Resources.P5);
+            list.Add(Resources.P9);
+            list.Add(Resources.P13);
+            list.Add(Resources.P17);
+            list.Add(Resources.P21);
+            list.Add(Resources.P25);
+            list.Add(Resources.P29);
+            list.Add(Resources.P33);
+            list.Add(Resources.P37);
+            list.Add(Resources.P41);
+            list.Add(Resources.P45);
+            list.Add(Resources.P49);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+         #endregion
+    }
+}
