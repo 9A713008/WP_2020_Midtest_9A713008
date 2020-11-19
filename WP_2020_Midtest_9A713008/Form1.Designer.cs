f45514ed4bb45f19c64173f7aea6427e073cca1d
張秤羿 <badthing0102@gmail.com>
2020-11-19 17:06:16 +0800

    增加文字,按鈕確定

--- WP_2020_Midtest_9A713008/Form1.Designer.cs
+++ WP_2020_Midtest_9A713008/Form1.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.btn1 = new System.Windows.Forms.Button();
             this.btn2 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pB1 = new System.Windows.Forms.PictureBox();
+            this.pB2 = new System.Windows.Forms.PictureBox();
             this.lab1 = new System.Windows.Forms.Label();
             this.btn3 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB2)).BeginInit();
             this.SuspendLayout();
             // 
             // btn1
@@ -58,36 +58,38 @@
             this.btn2.TabIndex = 1;
             this.btn2.Text = "玩家二";
             this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
             // 
-            // pictureBox1
+            // pB1
             // 
-            this.pictureBox1.Image = global::WP_2020_Midtest_9A713008.Properties.Resources.P0;
-            this.pictureBox1.Location = new System.Drawing.Point(90, 110);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(212, 193);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.pB1.Image = global::WP_2020_Midtest_9A713008.Properties.Resources.P0;
+            this.pB1.Location = new System.Drawing.Point(90, 110);
+            this.pB1.Name = "pB1";
+            this.pB1.Size = new System.Drawing.Size(212, 193);
+            this.pB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pB1.TabIndex = 2;
+            this.pB1.TabStop = false;
             // 
-            // pictureBox2
+            // pB2
             // 
-            this.pictureBox2.Image = global::WP_2020_Midtest_9A713008.Properties.Resources.P0;
-            this.pictureBox2.Location = new System.Drawing.Point(463, 110);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(197, 193);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
+            this.pB2.Image = global::WP_2020_Midtest_9A713008.Properties.Resources.P0;
+            this.pB2.Location = new System.Drawing.Point(463, 110);
+            this.pB2.Name = "pB2";
+            this.pB2.Size = new System.Drawing.Size(197, 193);
+            this.pB2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pB2.TabIndex = 3;
+            this.pB2.TabStop = false;
             // 
             // lab1
             // 
             this.lab1.AutoSize = true;
             this.lab1.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.lab1.Location = new System.Drawing.Point(138, 36);
+            this.lab1.Location = new System.Drawing.Point(172, 27);
             this.lab1.Name = "lab1";
-            this.lab1.Size = new System.Drawing.Size(69, 35);
+            this.lab1.Size = new System.Drawing.Size(399, 35);
             this.lab1.TabIndex = 4;
-            this.lab1.Text = "測試";
+            this.lab1.Text = "1~13張撲克牌,2為最大,3為最小";
+            this.lab1.Click += new System.EventHandler(this.lab1_Click);
             // 
             // btn3
             // 
@@ -98,6 +100,7 @@
             this.btn3.TabIndex = 5;
             this.btn3.Text = "reset";
             this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
             // 
             // Form1
             // 
@@ -106,8 +109,8 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btn3);
             this.Controls.Add(this.lab1);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pB2);
+            this.Controls.Add(this.pB1);
             this.Controls.Add(this.btn2);
             this.Controls.Add(this.btn1);
             this.Name = "Form1";
@@ -115,8 +118,8 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Shown += new System.EventHandler(this.Form1_Shown);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pB2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,8 +129,8 @@
 
         private System.Windows.Forms.Button btn1;
         private System.Windows.Forms.Button btn2;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pB1;
+        private System.Windows.Forms.PictureBox pB2;
         private System.Windows.Forms.Label lab1;
         private System.Windows.Forms.Button btn3;
     }
--- WP_2020_Midtest_9A713008/Form1.cs
+++ WP_2020_Midtest_9A713008/Form1.cs
@@ -21,26 +21,43 @@ namespace WP_2020_Midtest_9A713008
 
         private void btn1_Click(object sender, EventArgs e)
         {
-
+            Random a = new Random();//亂數種子
+            int i = a.Next(0, 14);
+            pB1.Image = list[i];
         }
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Random a = new Random();//亂數種子
+            int i = a.Next(0, 14);
+            pB2.Image = list[i];
+        }
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            pB1.Image = Resources.P0;
+            pB2.Image = Resources.P0;
+        }
+
         #region 生命週期
-        
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            list.Add(Resources.P0);
-            list.Add(Resources.P1);
-            list.Add(Resources.P5);
-            list.Add(Resources.P9);
-            list.Add(Resources.P13);
-            list.Add(Resources.P17);
-            list.Add(Resources.P21);
-            list.Add(Resources.P25);
-            list.Add(Resources.P29);
-            list.Add(Resources.P33);
-            list.Add(Resources.P37);
-            list.Add(Resources.P41);
-            list.Add(Resources.P45);
-            list.Add(Resources.P49);
+            #region 撲克牌
+            
+            list.Add(Resources.P0);//背面
+            list.Add(Resources.P1);//1
+            list.Add(Resources.P5);//2
+            list.Add(Resources.P9);//3
+            list.Add(Resources.P13);//4
+            list.Add(Resources.P17);//5
+            list.Add(Resources.P21);//6
+            list.Add(Resources.P25);//7
+            list.Add(Resources.P29);//8
+            list.Add(Resources.P33);//9
+            list.Add(Resources.P37);//10
+            list.Add(Resources.P41);//11
+            list.Add(Resources.P45);//12
+            list.Add(Resources.P49);//13
+            #endregion
         }
 
         private void Form1_Shown(object sender, EventArgs e)
@@ -52,6 +69,14 @@ namespace WP_2020_Midtest_9A713008
         {
 
         }
-         #endregion
+
+
+
+        #endregion
+
+        private void lab1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
